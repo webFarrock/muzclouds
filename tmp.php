2c7506675ac40913e7fac42d75391cdecb4abfdb
webFarrock <webFarrock@mail.ru>
2015-11-11 17:06:26 +0300

    test update tmp.php

--- tmp.php
+++ tmp.php
@@ -1,25 +1,6 @@
 
 <?
-$f1 = fopen( "f.txt" , "x+" );
 
-$f2 = fopen( "g.txt" , "x+" );
+die('hi');
 
-$text1 = fread( $f1 , filesize( "f.txt" ) );
-
-$text2= fread( $f2 , filesize( "g.txt" ) );
-
-
-
-print $text1; // Выведет содержимое файла.
-echo '<hr>';
-print $text2; // Выведет содержимое файла.
-
-
-fwrite($f1, $text2);
-fwrite($f2, $text1);
-
-
-
-fclose($f1);
-fclose($f2);
  
